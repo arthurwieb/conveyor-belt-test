12455c886435dc25a397c8108daab42fe8c1ac0b
arthur wiebusch <arthur.wieb@gmail.com>
2025-01-29 23:43:56 -0300

    adjust bug, need to clean code

--- scenes/belts/conveyor_belt_up.gd
+++ scenes/belts/conveyor_belt_up.gd
@@ -16,16 +16,12 @@ func _physics_process(delta):
 # Track bodies currently on the conveyor
 var overlapping_bodies := []
 
-func _ready():
-    # Connect signals to track when bodies enter and exit
-    body_entered.connect(_on_body_entered)
-    body_exited.connect(_on_body_exited)
-
 func _on_body_entered(body: Node2D):
+    print('body entered')
     if body.has_method("move_on_conveyor"):
         overlapping_bodies.append(body)
 
-func _on_body_exited(body: Node2D):
-    if body.has_method("move_on_conveyor"):
-        overlapping_bodies.erase(body)
 
+func _on_body_exited(body: Node2D) -> void:
+    print('body')
+    pass # Replace with function body.
--- scenes/items/iron_ore.gd
+++ scenes/items/iron_ore.gd
@@ -1,22 +1,36 @@
-extends RigidBody2D  # or CharacterBody2D
+extends CharacterBody2D # or CharacterBody2D
 
 var is_on_conveyor := false
 var conveyor_direction := Vector2.ZERO
 var conveyor_speed := 0.0
 
 func move_on_conveyor(direction: Vector2, speed: float, delta: float):
-    is_on_conveyor = true
-    conveyor_direction = direction
-    conveyor_speed = speed
+	is_on_conveyor = true
+	conveyor_direction = direction
+	conveyor_speed = speed
 
 func _physics_process(delta):
-    if is_on_conveyor:
-        # Move the item
-        position += conveyor_direction * conveyor_speed * delta
-    else:
-        # Stop moving when not on the conveyor
-        conveyor_direction = Vector2.ZERO
-        conveyor_speed = 0.0
+	if is_on_conveyor:
+		# Move the item
+		position += conveyor_direction * conveyor_speed * delta
+	else:
+		# Stop moving when not on the conveyor
+		conveyor_direction = Vector2.ZERO
+		conveyor_speed = 0.0
+	# print(is_on_conveyor)
+	# Reset the conveyor state for the next frame
+	is_on_conveyor = false
+	# print(is_on_conveyor)
 
-    # Reset the conveyor state for the next frame
-    is_on_conveyor = false
+
+func _on_body_entered(body: Node):
+	print('ore entrou')
+	pass # Replace with function body.
+
+
+func _on_mouse_entered() -> void:
+	print('cu')
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print('entrou body')
